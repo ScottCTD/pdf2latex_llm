20bd2b026c7d19ac662484780e3e9c14f7b5d657
Scott Cui <scottctd@outlook.com>
2025-10-03 21:24:51 -0400

    Add some utility scritps.

---
+++ pdf2latex/push_model_to_hub.py
@@ -0,0 +1,12 @@
+from huggingface_hub import HfApi, HfFolder, Repository, create_repo
+from huggingface_hub import upload_folder
+
+# 1. Create repo (public or private)
+create_repo("scottcfy/Qwen2-VL-2B-Instruct-pdf2latex", private=False)
+
+# 2. Upload folder
+upload_folder(
+    folder_path="./outputs/1/checkpoint-6000/merged",      # path to your saved model
+    repo_id="scottcfy/Qwen2-VL-2B-Instruct-pdf2latex",  # repo name on Hugging Face Hub
+    commit_message="Initial model upload 🚀"
+)
---
+++ scripts/serve.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source ~/miniconda3/etc/profile.d/conda.sh
+
+cd /u/scottc/pdf2latex_llm
+conda activate vllm
+
+vllm serve ./outputs/1/checkpoint-6000/merged/ \
+    --served-model-name Qwen2-VL-2B-Instruct-pdf2latex \
+    --port 8001 \
+    --gpu-memory-utilization 0.9
